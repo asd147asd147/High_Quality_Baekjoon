3c10077349a6957f9181fcddfd61b076248a935b
asd147asd147 <ds5vfr@gmail.com>
2021-03-25 18:02:16 +0900

    test

--- 13460-구슬 탈출 2.cpp
+++ 13460-구슬 탈출 2.cpp
@@ -5,14 +5,14 @@ using namespace std;
 typedef vector<vector<char>> vvc;
 typedef pair<int,int> pii;
 
-int N,M,ans=0;
-vvc vec(11,vector<char>(11));
+int N,M,ans=100;
 pair<int,int> O;
 void up(vvc,int,pii,pii);
 void down(vvc,int,pii,pii);
 void left(vvc,int,pii,pii);
 void right(vvc,int,pii,pii);
 void (*fp[4])(vvc, int, pii, pii) = {up, down, left, right};
+set<pair<pii,pii>> visited;
 
 bool check_pre(pii& R, pii& B, int c){
     if(c==0){
@@ -28,31 +28,161 @@ void up(vvc vec, int idx, pii R, pii B){
         return;
     pii pos[2] = {B,R};
     int sel = check_pre(R,B,1);
-    for(int i = pos[sel].first-1; i > 0; i--){
-        if(vec[i][pos[sel].second] == 'O' && sel == 1){
-            ans = max(ans,idx+1);
-            return;
+    int cnt = 0;
+    int tmp = 200;
+    while(cnt != 2){
+        for(int i = pos[sel].first-1; i > 0; i--){
+            if(vec[i][pos[sel].second] == 'O' && sel == 1){
+                tmp = idx+1;
+                vec[i+1][pos[sel].second] = '.';
+                break;
+            }
+            else if(vec[i][pos[sel].second] == 'O' && sel == 0){
+                return;
+            }
+            else if(vec[i][pos[sel].second] != '.'){
+                break;
+            }
+            else{
+                vec[i][pos[sel].second] = vec[i+1][pos[sel].second];
+                vec[i+1][pos[sel].second] = '.';
+                pos[sel].first = i;
+            }
         }
-        else if(vec[i][pos[sel].second] != '.'){
-            sel = sel;//TODO
-            break;
-        }
-        else{
-            pos[sel].first = i;
-        }
+        sel = !sel;
+        cnt++;
+    }
+    if(tmp != 200){
+        ans = min(ans,tmp);
+        return;
+    }
+    if(visited.find({pos[0],pos[1]})!=visited.end()){
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        fp[i](vec, idx+1, pos[1], pos[0]);
     }
 }
 void down(vvc vec, int idx, pii R, pii B){
     if(idx == 10)
         return;
+    pii pos[2] = {B,R};
+    int sel = !check_pre(R,B,1);
+    int cnt = 0;
+    int tmp = 200;
+    while(cnt != 2){
+        for(int i = pos[sel].first+1; i < N-1; i++){
+            if(vec[i][pos[sel].second] == 'O' && sel == 1){
+                tmp = idx+1;
+                vec[i-1][pos[sel].second] = '.';
+                break;
+            }
+            else if(vec[i][pos[sel].second] == 'O' && sel == 0){
+                return;
+            }
+            else if(vec[i][pos[sel].second] != '.'){
+                break;
+            }
+            else{
+                vec[i][pos[sel].second] = vec[i-1][pos[sel].second];
+                vec[i-1][pos[sel].second] = '.';
+                pos[sel].first = i;
+            }
+        }
+        sel = !sel;
+        cnt++;
+    }
+    if(tmp != 200){
+        ans = min(ans,tmp);
+        return;
+    }
+    if(visited.find({pos[0],pos[1]})!=visited.end()){
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        fp[i](vec, idx+1, pos[1], pos[0]);
+    }
 }
 void left(vvc vec, int idx, pii R, pii B){
     if(idx == 10)
         return;
+    pii pos[2] = {B,R};
+    int sel = check_pre(R,B,0);
+    int cnt = 0;
+    int tmp = 200;
+    while(cnt != 2){
+        for(int i = pos[sel].second-1; i > 0; i--){
+            if(vec[pos[sel].first][i] == 'O' && sel == 1){
+                tmp = idx+1;
+                vec[pos[sel].first][i+1] = '.';
+                break;
+            }
+            else if(vec[pos[sel].first][i] == 'O' && sel == 0){
+                return;
+            }
+            else if(vec[pos[sel].first][i] != '.'){
+                break;
+            }
+            else{
+                vec[pos[sel].first][i] = vec[pos[sel].first][i+1];
+                vec[pos[sel].first][i+1] = '.';
+                pos[sel].second = i;
+            }
+        }
+        sel = !sel;
+        cnt++;
+    }
+    if(tmp != 200){
+        ans = min(ans,tmp);
+
+        return;
+    }
+    if(visited.find({pos[0],pos[1]})!=visited.end()){
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        fp[i](vec, idx+1, pos[1], pos[0]);
+    }
 }
 void right(vvc vec, int idx, pii R, pii B){
     if(idx == 10)
         return;
+    pii pos[2] = {B,R};
+    int sel = !check_pre(R,B,0);
+    int cnt = 0;
+    int tmp = 200;
+    while(cnt != 2){
+        for(int i = pos[sel].second+1; i < M-1; i++){
+            if(vec[pos[sel].first][i] == 'O' && sel == 1){
+                tmp = idx+1;
+                vec[pos[sel].first][i-1] = '.';
+                break;
+            }
+            else if(vec[pos[sel].first][i] == 'O' && sel == 0){
+                return;
+            }
+            else if(vec[pos[sel].first][i] != '.'){
+                break;
+            }
+            else{
+                vec[pos[sel].first][i] = vec[pos[sel].first][i-1];
+                vec[pos[sel].first][i-1] = '.';
+                pos[sel].second = i;
+            }
+        }
+        sel = !sel;
+        cnt++;
+    }
+    if(tmp != 200){
+        ans = min(ans,tmp);
+        return;
+    }
+    if(visited.find({pos[0],pos[1]})!=visited.end()){
+        return;
+    }
+    for(int i = 0; i < 4; i++){
+        fp[i](vec, idx+1, pos[1], pos[0]);
+    }
 }
 
 int main(){
@@ -61,6 +191,7 @@ int main(){
 	cout.tie(0);
 	cin >> N >> M;
     pair<int,int> R,B;
+    vvc vec(11,vector<char>(11));
 	for(int i = 0; i < N; i++){
         string s;
         cin >> s;
@@ -77,7 +208,12 @@ int main(){
             }
         }
 	}
-
+	visited.insert({B,R});
+    for(int i = 0; i < 4; i++){
+        fp[i](vec, 0, R, B);
+    }
+    if(ans == 100) cout << -1;
+    else cout << ans;
 
 	return 0;
 }
