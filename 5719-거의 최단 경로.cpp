736856bd69ddb70d3a2ac3d67267c97ce4e2db99
asd147asd147 <asd147asd147@naver.com>
2021-09-30 18:25:12 +0900

    overwrite

--- 5719-거의 최단 경로.cpp
+++ 5719-거의 최단 경로.cpp
@@ -4,27 +4,38 @@ using namespace std;
 
 int N,M,S,E;
 vector<vector<pair<int,int>>> dist;
-vector<int> min_path, visited;
+vector<int> visited;
+vector<vector<int>> min_path;
 bool comp(const pair<int,int> &a, const pair<int,int> &b){
     return a.first < b.first;
 }
 
 int dijkstra(){
-    priority_queue<tuple<int,int,int>> pq;
-    pq.push({0,S,-1});
+    priority_queue<tuple<int,int,vector<int>>> pq;
+    int min_dist = 0;
+    pq.push({0,S,{S}});
     visited[S] = 1;
     while(!pq.empty()){
         auto p = pq.top();
-        min_path[get<1>(p)] = get<2>(p);
         if(get<1>(p) == E){
-            return -get<0>(p);
+            if(min_dist == 0){
+                min_path.push_back(get<2>(p));
+                min_dist = -get<0>(p);
+            }
+            else if(min_dist == -get<0>(p)){
+                min_path.push_back(get<2>(p));
+            }
+            else{
+                return -get<0>(p);
+            }
         }
         pq.pop();
         visited[get<1>(p)] = 1;
         for(auto d : dist[get<1>(p)]){
             if(visited[d.first]) continue;
             if(d.second == INF) continue;
-            pq.push({get<0>(p)-d.second,d.first,get<1>(p)});
+            get<2>(p).push_back(d.first);
+            pq.push({get<0>(p)-d.second,d.first,get<2>(p)});
         }
     }
     if(pq.empty()) return 0;
@@ -35,7 +46,7 @@ int dijkstra(){
 void find_path(int x){
     if(x == S) return;
     int idx = lower_bound(dist[min_path[x]].begin(), dist[min_path[x]].end(), make_pair(x,0), comp) - dist[min_path[x]].begin();
-    dist[min_path[x]][idx] = INF;
+    dist[min_path[x]][idx] = make_pair(x,INF);
     find_path(min_path[x]);
 }
 
@@ -45,10 +56,14 @@ void solve(){
     visited.clear();
     visited.resize(N+1);
     int minDist = dijkstra();
+    if(minDist == 0){
+        cout << -1 << "\n";
+        return;
+    }
     find_path(E);
     while(1){
         for(int i = 0; i <= N; i++){
-            min_path[i] = visited[i] = 0;
+            visited[i] = 0;
         }
         int curDist = dijkstra();
         if(curDist == 0){
